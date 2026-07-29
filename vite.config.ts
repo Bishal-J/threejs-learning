c4691e4525674513e03b09248477b96a9ab321da
bishal-j <bishal120101@gmail.com>
2026-07-29 13:37:47 +0530

    bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,23 @@ import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+
   build: {
     chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      input: Object.fromEntries(
-        globSync("src/pages/**/*.html").map((file) => {
-          const name = file.replace("src/pages/", "").replace(".html", "");
 
-          return [name, fileURLToPath(new URL(`./${file}`, import.meta.url))];
-        }),
-      ),
+    rollupOptions: {
+      input: {
+        index: fileURLToPath(new URL("./index.html", import.meta.url)),
+
+        ...Object.fromEntries(
+          globSync("src/pages/**/*.html").map((file) => {
+            const name = file.replace("src/pages/", "").replace(".html", "");
+
+            return [name, fileURLToPath(new URL(`./${file}`, import.meta.url))];
+          }),
+        ),
+      },
+
       output: {
         codeSplitting: true,
       },
