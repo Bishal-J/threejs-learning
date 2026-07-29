81b24333fa5d2998c8f775a956c000daf9f9aa1f
bishal-j <bishal120101@gmail.com>
2026-07-29 13:34:23 +0530

    fixed build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,23 @@
 import { defineConfig } from "vite";
+import { globSync } from "glob";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      input: Object.fromEntries(
+        globSync("src/pages/**/*.html").map((file) => {
+          const name = file.replace("src/pages/", "").replace(".html", "");
+
+          return [name, fileURLToPath(new URL(`./${file}`, import.meta.url))];
+        }),
+      ),
+      output: {
+        codeSplitting: true,
+      },
+    },
+  },
 });
